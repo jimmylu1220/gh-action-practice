09105bd7f1dd1f7c503d5e7ee0dc9729cfe5e8e1
jimmylu1220 <k8356218@gmail.com>
2023-08-02 22:14:51 +0800

    fix problem

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -1,12 +1,19 @@
-import "./HelpBox.css";
+import { string } from 'prop-types';
+
+import './HelpBox.css';
 
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <p>{text}</p>
     </article>
   );
 }
+
+HelpBox.propTypes = {
+  title: string,
+  text: string,
+};
 
 export default HelpBox;
